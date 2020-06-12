84bca1a894aeab7f0dcaf87c4fc2984be25d039f
cybranker <cybranker@yandex.ru>
2020-06-12 14:49:01 +0300

    Добавляет открытие/закрытие окна настроек, валидация ввода, изменения цвета частей мага, отправка формы

--- js/setup.js
+++ js/setup.js
@@ -36,9 +36,17 @@ var WIZARD_EYES_COLORS = [
   'yellow',
   'green'
 ];
+var WIZARD_FIREBALL_COLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+var MIN_NAME_LENGTH = 2;
+var MAX_NAME_LENGTH = 25;
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var setup = document.querySelector('.setup');
 var setupSimilarElement = document.querySelector('.setup-similar');
 setupSimilarElement.classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
@@ -83,3 +91,91 @@ var getFragmentWizards = function (wizards) {
 };
 
 similarListElement.appendChild(getFragmentWizards(getArrayWizards(QUANTITY_WIZARDS)));
+
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var userNameInput = document.querySelector('.setup-user-name');
+
+var popupEscPressHandler = function (evt) {
+  if (evt.key === 'Escape' && document.activeElement !== userNameInput) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', popupEscPressHandler);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+
+  document.removeEventListener('keydown', popupEscPressHandler);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    closePopup();
+  }
+});
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле huy');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function () {
+  var valueLength = userNameInput.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) + ' симв.');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+var setupWizardElement = document.querySelector('.setup-wizard');
+var wizardCoatElement = setupWizardElement.querySelector('.wizard-coat');
+var wizardEyesElement = setupWizardElement.querySelector('.wizard-eyes');
+var setupFireballElement = document.querySelector('.setup-fireball-wrap');
+var coatColorInput = document.querySelector('input[name="coat-color"]');
+var eyesColorInput = document.querySelector('input[name="eyes-color"]');
+var fireballColorInput = document.querySelector('input[name="fireball-color"]');
+
+var selectColorPartsWizard = function (part, color, input) {
+  part.style = ((part.matches('.setup-fireball-wrap')) ? 'background: ' : 'fill: ') + color + ';';
+  input.value = color;
+};
+
+wizardCoatElement.addEventListener('click', function () {
+  selectColorPartsWizard(wizardCoatElement, getArrayRandomElement(WIZARD_COAT_COLORS), coatColorInput);
+});
+
+wizardEyesElement.addEventListener('click', function () {
+  selectColorPartsWizard(wizardEyesElement, getArrayRandomElement(WIZARD_EYES_COLORS), eyesColorInput);
+});
+
+setupFireballElement.addEventListener('click', function () {
+  selectColorPartsWizard(setupFireballElement, getArrayRandomElement(WIZARD_FIREBALL_COLORS), fireballColorInput);
+});
